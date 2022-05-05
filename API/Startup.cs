13fbd85f4242f43d6916c3779beb532c72c0cfca
Rajesh <rgorijovula@alletec.com>
2022-05-05 11:58:59 +0530

    end of section 16

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
-
+ 
 namespace API
 {
     public class Startup
