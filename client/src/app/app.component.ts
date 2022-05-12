ff58c7115e31a08bc8db8dad3f6a1c2402449e34
Rajesh <rgorijovula@alletec.com>
2022-05-12 10:05:43 +0530

    upto vedio 195

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -11,10 +12,27 @@ export class AppComponent implements OnInit{
   title = 'SkiNet';
  
 
-  constructor(private basketService:BasketService ){}
+  constructor(private basketService:BasketService,private accountService:AccountService ){}
 
 
   ngOnInit(): void {
+   this.loadBasket();
+   this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token=localStorage.getItem('token')
+    if(token){
+      this.accountService.loadCurrentUser(token).subscribe(() =>{
+        console.log('loaded user');
+      },error =>{
+        console.log(error);
+      });
+    }
+
+  }
+
+  loadBasket(){
     const basketId=localStorage.getItem('basket_id');
     if (basketId)
     this.basketService.getBasket(basketId).subscribe(() =>{
@@ -24,3 +42,4 @@ export class AppComponent implements OnInit{
     });
   }
 }
+
